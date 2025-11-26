dae57389d0716fdb2800a2263df76d2c16d81073
AutoDL User <root@autodl.com>
2025-11-26 11:21:13 +0800

    modify TNN example codes to train in remote service

--- TNN/example/ex_5_1/dim5/ex_5_1_dim5.py
+++ TNN/example/ex_5_1/dim5/ex_5_1_dim5.py
@@ -8,14 +8,15 @@ from tnn import *
 
 import os
 import time
+import numpy as np
 
 pi = 3.14159265358979323846
 
 # ********** choose data type and device **********
 dtype = torch.double
 # dtype = torch.float
-device = 'cpu'
-# device = 'cuda'
+# device = 'cpu'
+device = 'cuda'
 
 
 # ********** generate data points **********
